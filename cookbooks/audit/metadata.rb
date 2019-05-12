4c2b658651f01b1637e011fd17eddd4089d498ff
Nicholas Mellen <nicholasmellen@gmail.com>
2019-05-12 15:19:00 -0400

    template files + dep cookbooks

---
+++ cookbooks/audit/metadata.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+name 'audit'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache-2.0'
+description 'Allows for fetching and executing compliance profiles, and reporting its results'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '7.5.0'
+
+source_url 'https://github.com/chef-cookbooks/audit'
+issues_url 'https://github.com/chef-cookbooks/audit/issues'
+
+chef_version '>= 12.20' if respond_to?(:chef_version)
+
+supports 'aix'
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'oracle'
+supports 'redhat'
+supports 'suse'
+supports 'opensuse'
+supports 'opensuseleap'
+supports 'ubuntu'
+supports 'windows'
